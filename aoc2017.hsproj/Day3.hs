96c7fd471747a518c192d8cd5c95d39cf89d45b1
Sami Laitala <sami.laitala@gmail.com>
2017-12-07 13:24:58 +0200

    Day3 part2

--- aoc2017.hsproj/Day3.hs
+++ aoc2017.hsproj/Day3.hs
@@ -3,6 +3,7 @@ module Day3 where
 import Prelude hiding (Right, Left)
 
 data Direction = Right | Up | Left | Down deriving (Show)
+
 data Point = Point Int Int deriving (Show)
 
 directions = 
@@ -21,5 +22,36 @@ getCoords steps = foldl (flip move) (Point 0 0) $ take steps directions
 
 stepsToHome afterSteps = abs x + abs y
   where (Point x y) = getCoords $ pred afterSteps
+  
+type Value = Int
+data ValuePoint = ValuePoint Point Value deriving (Show)
 
+
+calculateValuePoint :: [ValuePoint] -> Point -> ValuePoint
+calculateValuePoint valuePoints (Point x y) =
+  let deltas = [(dx,dy)| dx <- [-1, 0, 1], dy <- [-1, 0, 1], [dx, dy] /= [0,0]]
+      neighbourCoors = map (\(dx,dy) -> (x+dx, y+dy)) deltas
+      neighbours = filter (\ (ValuePoint (Point x' y') _) -> any (== (x', y')) neighbourCoors) valuePoints 
+      value = foldl (\ sum (ValuePoint _ v) -> sum + v) 0 neighbours
+  in ValuePoint (Point x y) value
+  
+valuePoints = 
+  [ ValuePoint (Point 0 0) 1
+  , ValuePoint (Point 1 0) 1
+  , ValuePoint (Point 1 1) 2
+  ]
+  
+findBiggerValue :: [ValuePoint] -> [Direction] -> Int -> Int
+findBiggerValue values directions thresholdValue = 
+  let nextDirection = head directions
+      ValuePoint currentPoint _ = last values
+      nextPoint = move nextDirection currentPoint
+      newValuePoint@(ValuePoint _ value) = calculateValuePoint values nextPoint
+  in
+    if value > thresholdValue
+    then value
+    else findBiggerValue (values ++ [newValuePoint]) (tail directions) thresholdValue
+
+part2 :: Int -> Int
+part2 = findBiggerValue [ValuePoint (Point 0 0) 1] directions
 
